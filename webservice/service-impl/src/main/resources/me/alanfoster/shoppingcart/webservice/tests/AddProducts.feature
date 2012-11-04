683f646c33c8bac72a84fe94271b0cb55a8c5bcd
Alan Foster <alan@alanfoster.me>
2012-11-04 22:07:56 +0000

    Add test steps for adding products to shopping cart  behaviour test

--- webservice/service-impl/src/main/resources/me/alanfoster/shoppingcart/webservice/tests/AddProducts.feature
+++ webservice/service-impl/src/main/resources/me/alanfoster/shoppingcart/webservice/tests/AddProducts.feature
@@ -1,24 +1,25 @@
 Feature: Adding Products to a customer's shopping cart
-	As an Api user
-	I want to be able to products to a customer's account
-	In order to keep track of what a customer wants to order
-		
-Scenario: Adding a product to an shopping cart
-	Given the Api has the following list of customers with empty shopping carts
-		| Id | Email  		 | Password    	 | 
-		| 1	 | alan@foo.com  | rawTextForNow |
-	And the Api has the following products
-		| id | Name 				| Description			| Price |
-		| 1  | Duracell AA Battery	| Lasts longer			| 4f	|
-		| 2  | "Milk				| Only the freshest		| 0.89f |
-		| 3  | "Eggs				| Free Range			| 1.19f |
-		| 4  | "Bread				| 3 day life			| 1.20f |
-	When I call the addProductToCustomerAccount operation with the following information
-		| CustomerId | ProductId | Quantity |
-		| 1			 | 2		 | 1		|
-	Then the returned customer shall have the following core information
-		| Id | Email  		 | Password    	 | 
-		| 1	 | alan@foo.com  | rawTextForNow |
-	And the returned customer shall have the following shopping cart
-		| Product Name 	   | Quantity |
-		| Milk			   | 2 		|
+  As an Api user
+  I want to be able to products to a customer's account
+  In order to keep track of what a customer wants to order
+
+  Scenario: Adding a product to an shopping cart
+    Given the Api has the following list of customers with empty shopping carts
+      | CustomerId | Email        | Password      |
+      | 1          | alan@foo.com | rawTextForNow |
+    And the Api has the following products
+      | ProductId | Name                | Description        | Price |
+      | 1         | Duracell AA Battery | Lasts longer       | 4     |
+      | 2         | Milk                | Only the freshest  | 0.89  |
+      | 3         | Eggs                | Free Range         | 1.19  |
+      | 4         | Bread               | 3 day life         | 1.20  |
+    When I call the addProductToCustomerAccount operation with the following information
+      | CustomerId | ProductId | Quantity |
+      | 1          | 2         | 1        |
+    Then there will be no errors given in the response
+    And the returned customer shall have the following core information
+      | CustomerId | Email        | Password      |
+      | 1          | alan@foo.com | rawTextForNow |
+    And the returned customer shall have the following shopping cart
+      | ProductId | Name                | Description       | Price | Quantity |
+      | 2         | Milk                | Only the freshest | 0.89  | 1        |
--- webservice/service-impl/src/test/java/me/alanfoster/shoppingcart/webservice/tests/Steps.java
+++ webservice/service-impl/src/test/java/me/alanfoster/shoppingcart/webservice/tests/Steps.java
@@ -1,18 +1,101 @@
 package me.alanfoster.shoppingcart.webservice.tests;
 
+import java.math.BigInteger;
+import java.util.List;
+import java.util.Map;
+
+import me.alanfoster.shoppingcart.webservice.ShoppingCartPortTypeImpl;
+import me.alanfoster.shoppingcart.webservice.util.CustomerAssert;
+import me.alanfoster.shoppingcart.webservice.util.CustomerFactory;
+import me.alanfoster.shoppingcart.webservice.util.ProductFactory;
+import me.alanfoster.tests.shoppingcart.wsdl.proxyclasses.AddProductToCustomerAccountRequest;
+import me.alanfoster.tests.shoppingcart.wsdl.proxyclasses.AddProductToCustomerAccountResponse;
+import me.alanfoster.tests.shoppingcart.wsdl.proxyclasses.CartItemType;
+import me.alanfoster.tests.shoppingcart.wsdl.proxyclasses.CustomerType;
+import me.alanfoster.tests.shoppingcart.wsdl.proxyclasses.ProductType;
+import me.alanfoster.tests.shoppingcart.wsdl.proxyclasses.ShoppingCart;
+import me.alanfoster.tests.shoppingcart.wsdl.proxyclasses.ShoppingCartType;
+
+import cucumber.api.DataTable;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import cucumber.runtime.PendingException;
 
 import static junit.framework.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class Steps {
-	@Given("^this works$")
-	public void given_this_works() throws Throwable {
-		assertTrue(true);
+	
+	private ShoppingCartPortTypeImpl shoppingCartPortTypeImpl;
+	
+	private AddProductToCustomerAccountResponse response;
+	
+	public ShoppingCartPortTypeImpl getShoppingCartPortType() {
+		if(shoppingCartPortTypeImpl == null) {
+			shoppingCartPortTypeImpl = new ShoppingCartPortTypeImpl();
+		}
+		return shoppingCartPortTypeImpl;
+	}
+	
+	@Given("^the Api has the following list of customers with empty shopping carts$")
+	public void the_Api_has_the_following_list_of_customers_with_empty_shopping_carts(List<CustomerType> customers) throws Throwable {
+		for(CustomerType customer : customers) {
+			customer.setShoppingCart(new ShoppingCartType());
+		}
+		getShoppingCartPortType().setCustomers(customers);
 	}
 
-	@Then("^i shall be happy$")
-	public void then_this_works() throws Throwable {
-		assertTrue(true);
+	@Given("^the Api has the following products$")
+	public void the_Api_has_the_following_products(List<ProductType> products) throws Throwable {
+		getShoppingCartPortType().setProducts(products);
+	}
+
+	@When("^I call the addProductToCustomerAccount operation with the following information$")
+	public void I_call_the_addProductToCustomerAccount_operation_with_the_following_information(List<AddProductToCustomerAccountRequest> requests) throws Throwable {
+		response = getShoppingCartPortType().addProductToCustomerAccount(requests.get(0));
+	}
+	
+
+	@Then("^there will be no errors given in the response$")
+	public void there_will_be_no_errors_given_in_the_response() throws Throwable {
+	   assertFalse("There should be no error in the response", response.isError());
+	   assertNull("There should be no error description in the response", response.getErrorReason());
+	}
+
+	@Then("^the returned customer shall have the following core information$")
+	public void the_returned_customer_shall_have_the_following_core_information(DataTable customers) throws Throwable {
+		CustomerType actualCustomer = response.getCustomer();
+		
+		Map<String, String> data = customers.asMaps().get(0);
+		String expectedCustomerId = data.get("CustomerId");
+		String expectedEmail = data.get("Email");
+		String expectedPassword = data.get("Password");
+		
+		assertNotNull("There should have been a customer object returned", actualCustomer);
+    	assertEquals("Both customers must have the same customer id", expectedCustomerId, actualCustomer.getCustomerId());
+    	assertEquals("Both customers must have the same email", expectedEmail, actualCustomer.getEmail());
+    	assertEquals("Both customers must have the same password", expectedPassword, actualCustomer.getPassword());
+	}
+
+	@Then("^the returned customer shall have the following shopping cart$")
+	public void the_returned_customer_shall_have_the_following_shopping_cart(DataTable datatable) throws Throwable {
+		// Construct the expected shopping cart
+		ShoppingCartType expectedShoppingCart = new ShoppingCartType();
+		List<Map<String, String>> maps = datatable.asMaps();
+		for(Map<String, String> map : maps) {
+			CartItemType cartItem = new CartItemType();
+			cartItem.setQuantity(new BigInteger(map.get("Quantity")));
+			cartItem.setProduct(
+					ProductFactory.getNewProduct(
+							map.get("ProductId"), map.get("Name"), map.get("Description"), Float.parseFloat(map.get("Price")
+					))
+			);
+			expectedShoppingCart.getCartItem().add(cartItem);
+		}
+		
+		ShoppingCartType actualShoppingCart = response.getCustomer().getShoppingCart();
+		assertNotNull("There should have been a shopping cart object returned", actualShoppingCart);
+		CustomerAssert.assertEqual(expectedShoppingCart, actualShoppingCart);
 	}
 }
